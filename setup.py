3bf4b183202b211cb898e8f81a97786aff0cb8f4
Bryerton <bryerton@triumf.ca>
2019-01-28 13:37:49 -0800

    changd pyreadline to readline

--- setup.py
+++ setup.py
@@ -75,7 +75,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['requests','argparse','future','pyreadline'],
+    install_requires=['requests','argparse','future','readline'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
